7306e43a0af57632ce861c2860b95cd9b603c144
Liis U <liis.usin@gmail.com>
2022-06-30 09:40:09 +0200

    my solution + 2 other solutions from Leetcode

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -17,10 +17,28 @@ class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """Two sum function."""
+        # # My solution - Approach 1: Brute Force
         for i in range(0, len(nums)):
             for j in range(len(nums)-1, 0, -1):
                 if nums[i] + nums[j] == target and not i == j:
                     return [i, j]
+
+        # # Approach 2: Two-pass Hash Table
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]] = i
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap and hashmap[complement] != i:
+        #         return [i, hashmap[complement]]
+
+        # # Approach 3: One-pass Hash Table
+        # hashmap = {}
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap:
+        #         return [i, hashmap[complement]]
+        #     hashmap[nums[i]] = i
 
 
 def main():
