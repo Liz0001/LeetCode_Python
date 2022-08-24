2cadc88ed2489d38e544d84855c655471629ed9d
Liis U <liis.usin@gmail.com>
2022-08-24 10:59:19 +0200

    fix

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,5 +1,5 @@
 """
-1. Two Sum form Leetcode.
+1. Two Sum from Leetcode.
 
 Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
