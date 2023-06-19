422183854c3586cddfb04c10193885aa38eeca7b
Liis U <liis.usin@gmail.com>
2023-06-19 15:16:43 +0200

    Palindrome number LEETCODE faster solution

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -1,16 +1,20 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if (x < 0):
-            return False
-        xList = list(map(int, str(x)))
-        j = len(xList)
+        # if (x < 0):
+        #     return False
+        # xList = list(map(int, str(x)))
+        # j = len(xList)
         
-        for i in range(0, (len(xList)//2)):
-            j = j-1
-            if xList[i] != xList[j]:
-                return False
-        return True
+        # for i in range(0, (len(xList)//2)):
+        #     j = j-1
+        #     if xList[i] != xList[j]:
+        #         return False
+        # return True
+        y = int((str(x)[::-1]))
+        if x == y:
+            return True
+        return False
 
 
 def main():
