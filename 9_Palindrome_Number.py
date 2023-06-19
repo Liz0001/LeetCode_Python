c3928f294c4f640cdc3b137d060c5709265cea51
Liis U <liis.usin@gmail.com>
2023-06-19 15:16:12 +0200

    Palindrome number LEETCODE

---
+++ 9_Palindrome_Number.py
@@ -0,0 +1,24 @@
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if (x < 0):
+            return False
+        xList = list(map(int, str(x)))
+        j = len(xList)
+        
+        for i in range(0, (len(xList)//2)):
+            j = j-1
+            if xList[i] != xList[j]:
+                return False
+        return True
+
+
+def main():
+    """Solution."""
+    sol = Solution()
+    num = 1221
+    print(sol.isPalindrome(num))
+
+
+if __name__ == "__main__":
+    main()
