042f4ba63a5af296a9cea155cdf432c8da6c5701
Liis U <liis.usin@gmail.com>
2023-06-25 16:28:54 +0200

    remove element

--- 27_Remove_Element.py
+++ 27_Remove_Element.py
@@ -2,13 +2,10 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # print([value for value in nums if value != val])
-        print(nums)
         
         while val in nums:
             nums.remove(val)
         
-        print(nums)
         return len(nums)
 
 def main():
