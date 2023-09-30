6b15c05b744b9e7b0196932d50a96d24c0032b42
Liis U <liis.usin@gmail.com>
2023-09-30 12:43:11 +0200

    Search in a Binary Search Tree (runtime 94.82%)

--- 700_Search_in_a_Binary_Search_Tree.py
+++ 700_Search_in_a_Binary_Search_Tree.py
@@ -12,7 +12,7 @@ class Solution:
     def searchBST(self, root: ['TreeNode'], val: int) -> ['TreeNode']:
         if root is None:
             return None
-        if val > root.val:
+        elif val > root.val:
             return self.searchBST(root.right, val)
         elif val < root.val:
             return self.searchBST(root.left, val)
