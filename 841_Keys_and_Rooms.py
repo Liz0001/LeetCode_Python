1187088e7b70cf4c0e66cab66a983529e158cac2
Liis U <liis.usin@gmail.com>
2023-11-26 22:12:26 +0100

    Keys and Rooms (runtime 28.26%)

--- 841_Keys_and_Rooms.py
+++ 841_Keys_and_Rooms.py
@@ -1,13 +1,19 @@
-import unittest
 from typing import List
+import unittest
 
 
 class Solution:
-    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+    def canVisitAllRooms(self, r: List[List[int]]) -> bool:
+        rooms = {}
+        visited = [0]
+        for i in range(len(r)):
+            rooms[i] = r[i]
         
-
-
-
+        for i in visited:
+            for j in rooms.get(i):
+                if j not in visited:
+                    visited.append(j)
+        return len(set(visited)) == len(r)
 
 
 class TestSolution(unittest.TestCase):
@@ -19,12 +25,17 @@ class TestSolution(unittest.TestCase):
         rooms = [[1],[2],[3],[]]
         exp = True
         out = sol.canVisitAllRooms(rooms)
-        # self.assertEqual(out, exp)
+        self.assertEqual(out, exp)
         
-        # rooms = [[1,3],[3,0,1],[2],[0]]
-        # exp = True
-        # out = sol.canVisitAllRooms(rooms)
-        # self.assertEqual(out, exp)
+        rooms = [[1,3],[3,0,1],[2],[0]]
+        exp = False
+        out = sol.canVisitAllRooms(rooms)
+        self.assertEqual(out, exp)
+        
+        rooms = [[2],[],[1]]
+        exp = True
+        out = sol.canVisitAllRooms(rooms)
+        self.assertEqual(out, exp)
 
 
 if __name__ == "__main__":
