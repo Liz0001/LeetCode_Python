00578c960b02589946b22ac800faa5ad8fe569ef
Liis U <liis.usin@gmail.com>
2023-12-14 12:26:29 +0100

    Strictly Palindromic Number (runtime beats 7.73%)

--- 2396_Strictly_Palindromic_Number.py
+++ 2396_Strictly_Palindromic_Number.py
@@ -3,21 +3,9 @@ import unittest
 
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        binary = bin(n)
-        b = binary.split('b')[1]
-        bb = list(b)
-        dd = bb[::-1]
-        print()
-        print(list(b))
-        print()
-        print(list(b)[:2],(list(b)[:-2])[::-1])
-        for i in range(2, n-1):
-            if list(b)[:2] != (list(b)[:-2])[::-1]:
-                return False
-        # print(list(b)[:2] == (list(b)[:-2])[::-1])
-        
-        
-        
+        binary = bin(n).split('b')[1]
+        if list(binary)[:2] != (list()[:-2])[::-1]:
+            return False
         return True
 
 
